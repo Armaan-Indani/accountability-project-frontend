5ecca20ae968482044012c5629053d9c9f113ef2
Armaan <indaniarmaan4@gmail.com>
2025-01-03 19:56:42 +0530

    feat: cleanliness

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,6 @@ import HomePage from "./pages/homepage.tsx";
 function App() {
   return (
     <div className="App">
-      {/* <h1 className="text-3xl font-bold underline text-red-600">
-        Simple React Typescript Tailwind Sample
-      </h1> */}
       <HomePage />
     </div>
   );
